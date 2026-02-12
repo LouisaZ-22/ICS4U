88d48e6446c7242ca30217d8ac4a179b099a4f69
Louisa Zhang <lzhang@bayviewglen.ca>
2026-02-12 10:54:47 -0500

    edited index.html scripts

--- dom_project/user/model.js
+++ dom_project/user/model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     name: {type: String, required: true},
-    phone: {type: String, required: true, match: /\(\d{3}\)\d{3}-\d{4}/},
+    phone: {type: String, required: true, match: /\(\d{3}\) \d{3}-\d{4}/},
     address: {type: String, required: true},
     gender: {type: String, required: true},
     age: {type: Number, required: true},
